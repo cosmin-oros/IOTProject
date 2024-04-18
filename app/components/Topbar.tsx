63fd0bfb4bc4ba4639d9e34f135e1387353ab2b7
Cosmin <oroscosmin09@gmail.com>
2024-04-18 14:28:39 +0300

    topbar of homescreen

---
+++ app/components/Topbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons';
+
+interface TopBarProps {
+  onLogOutPress: () => void;
+  title: string;
+  onSettingsPress: () => void;
+}
+
+const TopBar: React.FC<TopBarProps> = ({ onLogOutPress, title, onSettingsPress }) => {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={onLogOutPress}>
+          <AntDesign name="logout" size={24} color="black" />
+        </TouchableOpacity>
+        
+        <Text style={styles.title}>{title}</Text>
+
+        <TouchableOpacity onPress={onSettingsPress}>
+          <Ionicons name="settings" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
+
+export default TopBar;
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,17 +1,40 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { Platform, SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
+import TopBar from "../components/Topbar";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RouteParams } from "../routes/types";
+import { Routes } from "../routes/routes";
+import { useNavigation } from "@react-navigation/native";
+
+type RoutePropType = StackNavigationProp<RouteParams, Routes.Home>;
 
 const HomeScreen: React.FC = () => {
+  const navigation = useNavigation<RoutePropType>();
+
+  const handleLogOutPress = () => {
+    // ! change the variable in the store to isLoggedIn false
+    navigation.navigate(Routes.Register);
+  };
+
+  const handleSettingsPress = () => {
+    navigation.navigate(Routes.Settings);
+  };
   
   return (
-    <SafeAreaView>
-      <View></View>
+    <SafeAreaView style={styles.content}>
+      <TopBar
+        onLogOutPress={handleLogOutPress}
+        title="Title of the app"
+        onSettingsPress={handleSettingsPress}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  content: {
+    flex: 1,
+  },
 });
 
 
