6380797e57f0d4c3a8dce287c33265e4eb848a8e
Cosmin <oroscosmin09@gmail.com>
2024-05-12 17:59:57 +0300

    table init

--- web/src/pages/TablePage.tsx
+++ web/src/pages/TablePage.tsx
@@ -1,11 +1,86 @@
-import React from 'react';
-import '../styles.css'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PageRoutes } from '../routes/PageRoutes';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
+import { format } from 'date-fns';
+import { Timestamp } from 'firebase/firestore';
 
-const TablePage = () => {
+type TableDataRow = {
+  timestamp: string;
+  isOn: string;
+};
+
+const TablePage: React.FC = () => {
+  const navigate = useNavigate();
+  const [tableData, setTableData] = useState<TableDataRow[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const db = getFirestore();
+        const ledStateCollectionRef = collection(db, 'ledState');
+        const q = query(ledStateCollectionRef, orderBy('timestamp', 'asc'));
+
+        onSnapshot(q, (querySnapshot) => {
+          const newData: TableDataRow[] = [];
+          querySnapshot.forEach((doc) => {
+            const data = doc.data() as { isOn: boolean; timestamp: Timestamp };
+            if (data.isOn) {
+              newData.push({
+                timestamp: formatDate(data.timestamp.toDate().getTime()),
+                isOn: data.isOn ? 'On' : 'Off',
+              });
+            }
+          });
+          setTableData(newData);
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleBack = () => {
+    navigate(PageRoutes.Main);
+  };
+
+  const formatDate = (timestamp: number): string => {
+    if (isNaN(timestamp) || timestamp < 0) {
+      return ''; // Return an empty string for invalid timestamps
+    }
+    return format(new Date(timestamp), 'yyyy-MM-dd'); // Format valid timestamps using date-fns
+  };
 
   return (
     <div className='main-container'>
-      
+      <div className='main-header'>
+        <button className='back-btn' onClick={handleBack}>
+          Back
+        </button>
+      </div>
+      <div className='content'>
+        <div className='table-container'>
+          <table>
+            <thead>
+              <tr>
+                <th>Timestamp</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((row, index) => (
+                <tr key={index}>
+                  <td>{row.timestamp}</td>
+                  <td>{row.isOn}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
